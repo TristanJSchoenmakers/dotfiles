f80c04dfc988d09dbd68363647673f496332c5ed
Tristan J. Schoenmakers <Tristansjh@gmail.com>
2022-11-24 21:39:24 +0100

    update

---
+++ i3/polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main -c "~/.config/i3/polybar/polybar.config" &
+  done
+else
+  polybar --reload mybar &
+fi
+
+echo "Polybar launched..."
