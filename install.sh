ed16c4c18284b9b9025811ccc19d33e5ed82a403
Tristan J. Schoenmakers <tristan.schoenmakers@sneleentaxi.nl>
2025-05-20 16:11:51 +0200

    Move bash init to configuration.nix

--- install.sh
+++ install.sh
@@ -6,5 +6,3 @@ git clone https://github.com/TristanJSchoenmakers/dotfiles.git
 mv $HOME/.config $HOME/.config-old && mv dotfiles $HOME/.config
 sudo mv /etc/nix/configuration.nix /etc/nixos/configuration-old.nix
 sudo ln -s $HOME/.config/nix/configuration.nix /etc/nixos/configuration.nix
-
-echo "source ~/.config/bash/.bashrc" > ~/.bashrc
