2c85c56ee92d0c00fd4669525fe7dff00327a2fa
Tristan J. Schoenmakers <tristansjh@gmail.com>
2025-08-28 17:52:19 +0200

    Hyprland cursor size & theme fixes

--- nix/desktop.nix
+++ nix/desktop.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   services.xserver = {
@@ -50,6 +50,26 @@
     enable = true;
     xwayland.enable = true;
   };
+
+  # Cursor / theme fix: https://wiki.hypr.land/Nix/Hyprland-on-NixOS/#fixing-problems-with-themes & https://wiki.hypr.land/Hypr-Ecosystem/hyprcursor/
+  environment.sessionVariables = {
+    XCURSOR_THEME   = "Adwaita";
+    XCURSOR_SIZE    = 24;
+    HYPRCURSOR_SIZE = 24;
+  };
+
+  programs.dconf.profiles.user.databases = [
+    {
+      settings."org/gnome/desktop/interface" = {
+        gtk-theme = "Adwaita";
+        icon-theme = "Flat-Remix-Red-Dark";
+        font-name = "Noto Sans Medium 11";
+        # cursor-size = lib.gvariant.mkInt32 64;
+        document-font-name = "Noto Sans Medium 11";
+        monospace-font-name = "Noto Sans Mono Medium 11";
+      };
+    }
+  ];
 
   environment.systemPackages = with pkgs; [
     # display
