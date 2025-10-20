a60a95e913124f7d78553a24639cdead911b82c1
TristanJSchoenmakers <tristansjh@gmail.com>
2025-10-20 14:52:36 +0200

    Add nix configuration extension for 'home'

---
+++ nix/home.nix
@@ -0,0 +1,18 @@
+{ inputs, config, pkgs, ... }:
+
+let
+  secrets = if builtins.pathExists ./secrets.nix
+              then import ./secrets.nix
+              else {};
+in
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  };
+
+  environment.systemPackages = with pkgs; [
+    discord
+    qbittorrent-qt5
+  ];
+}
