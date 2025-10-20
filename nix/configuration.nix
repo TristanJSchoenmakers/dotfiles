73be5e0a8087d3dc4e8034a8bf6fb6b830de6930
TristanJSchoenmakers <tristansjh@gmail.com>
2025-10-20 14:52:37 +0200

    update

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -182,12 +182,6 @@ in
     slack
   ];
 
-  #environment.variables.LD_LIBRARY_PATH = builtins.concatStringsSep ":" [
-  #  "${pkgs.xorg.libX11}/lib"
-  #  "${pkgs.xorg.libXi}/lib"
-  #  "${pkgs.libGL}/lib"
-  #];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
