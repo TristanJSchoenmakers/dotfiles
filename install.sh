f9ff5e25fe45f6269bbd1bb0b58fc9f2863fcad7
Tristan J. Schoenmakers <tristansjh@gmail.com>
2026-01-07 18:58:22 +0100

    Small fixes

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@
 nix-shell -p git
 git clone https://github.com/TristanJSchoenmakers/dotfiles.git
 mv $HOME/.config $HOME/.config-old && mv dotfiles $HOME/.config
-sudo mv /etc/nix/configuration.nix /etc/nixos/configuration-old.nix
+sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration-old.nix
 sudo ln -s $HOME/.config/nix/configuration.nix /etc/nixos/configuration.nix
